6c3cfb2e3206c6fd51308859e3b777f17dbed9bc
KamdemE <206837425+KamdemE@users.noreply.github.com>
2026-09-23 07:37:50 +0000

    Update: Matrix-Vector Dot Product

--- problems/0001-matrix-vector-dot-product/solution.py
+++ problems/0001-matrix-vector-dot-product/solution.py
@@ -1,12 +1,10 @@
+import numpy as np
+
 def matrix_dot_vector(a: list[list[int|float]], b: list[int|float]) -> list[int|float]:
 	if len(a[0])!=len(b):
 		return -1
 	else:
-		M=[]
-		for i in range(len(a)):
-			sums=0
-			for j in range(len(a[0])):
-		   		sums=sums+a[i][j]*b[j]
-			M.append(sums)
-		return M
+		a=np.array(a)
+		b=np.array(b)
+		return list(a@b)
 			
